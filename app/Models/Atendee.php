1570844a79e7bc6216f9e029a39f7ee68f898b24
maciekstudzinski <maciek@hrefagency.co.uk>
2024-02-21 15:06:53 +0000

    Specified relationships between models

--- app/Models/Atendee.php
+++ app/Models/Atendee.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * User relation - many to one
+ * Event relation - many to one
+ */
 
 class Atendee extends Model
 {
     use HasFactory;
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event(): BelongsTo {
+        return $this->belongsTo(Event::class);
+    }
 }
--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ * User relationship - many to one
+ * Atendee relationship - one to many
+ */
 
 class Event extends Model
 {
     use HasFactory;
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function attendees(): HasMany {
+        return $this->hasMany(Atendee::class);
+    }
+
 }
